fd265f5a2498e3e481369fa97f1446afe223b179
Alex <jcotamomoli@uniminuto.edu.co>
2023-09-20 21:03:31 -0500

    Primer commit

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,12 +1,20 @@
+// function getMessage(saludo) {
+//   return `Hola , ${saludo}`
+// }
 
-export const FirstApp = () => {
+export const FirstApp = ({title}) => {
+  
+  if (!title) {
+    throw new Error("El titulo no existe");
+  }
   return (
-    //esto es un fragmento <> 
-    
-    <> 
-    <h1>FirstApp</h1>
-    <p>Cotamo</p>
-    
+    //esto es un fragmento <>
+
+    <>
+      <h1>{title}</h1>
+      {/* <h1>{JSON.stringify(getMessage('john'))}</h1> */}
+      {/* <code>{JSON.stringify(newMessage) }</code> */}
+      <p>Cotamo</p>
     </>
-  )
-}
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./HelloEorlApp";
+// import { App } from "./HelloEorlApp";
 import { FirstApp } from "./FirstApp";
+import "./styles.css";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <FirstApp/>
-        <App/>
-    </React.StrictMode>
-)
-
-
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <FirstApp/>
+    {/* <App/> */}
+  </React.StrictMode>
+);
