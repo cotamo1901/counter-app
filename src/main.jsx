b9b53e598638a7580f07e14ff0b8e62c5d305775
Alex <jcotamomoli@uniminuto.edu.co>
2023-09-20 22:48:23 -0500

    Primer commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 // import { App } from "./HelloEorlApp";
-import { FirstApp } from "./FirstApp";
+// import { FirstApp } from "./FirstApp";
+import {CounterApp} from "./CounterApp"
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <FirstApp />
+    <CounterApp value={111}/>
+    {/* <FirstApp /> */}
     {/* <App/> */}
   </React.StrictMode>
 );
