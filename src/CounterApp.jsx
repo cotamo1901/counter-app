795cd878b99de6bd302a880cfdc94e99eacd93f7
Alex <jcotamomoli@uniminuto.edu.co>
2023-09-21 21:56:13 -0500

    Primer commit

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,15 +1,28 @@
-import PropTypes from "prop-types";
+import { useState } from "react";
+import PropTypes, { func } from "prop-types";
 
 export const CounterApp = ({ value }) => {
-  function handleAdd(event) {
-    console.log(event);
-  }
+  const [counter, seCounter] = useState(value);
+
+  const handleAdd = () => {
+    seCounter(counter + 1);
+  };
+
+  const handleRemove = () => {
+    seCounter(counter - 1);
+  };
+
+  const handleReset = () => {
+    seCounter(value);
+  };
 
   return (
     <>
       <h1>CounterApp</h1>
-      <h2>{value}</h2>
+      <h2>{counter}</h2>
       <button onClick={handleAdd}>+1</button>
+      <button onClick={handleRemove}>-1</button>
+      <button onClick={handleReset}>Reset</button>
     </>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <CounterApp value={111}/>
+    <CounterApp value={0}/>
     {/* <FirstApp /> */}
     {/* <App/> */}
   </React.StrictMode>
