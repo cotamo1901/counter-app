6748f4932b871db690e3a3121f54908644e1b2a7
john cotamo <johncotamo@hotmail.com>
2023-09-25 16:38:05 -0500

    se añade fetch

--- tests/base-pruebas/05-funciones.test.js
+++ tests/base-pruebas/05-funciones.test.js
@@ -1,17 +1,20 @@
-import { getUser } from "../../src/base-pruebas/05-funciones"
+import { getUser, getUsuarioActivo } from "../../src/base-pruebas/05-funciones";
 
-describe('Pruebas en 05-funciones', () => { 
+describe("Pruebas en 05-funciones", () => {
+  test("getUser debe retornar un objeto", () => {
+    const testUser = {
+      uid: "ABC123",
+      username: "El_Papi1502",
+    };
+    const user = getUser();
 
-    test('getUser debe retornar un objeto', () => {
+    expect(testUser).toEqual(user);
+  });
 
-        const testUser ={
-            uid: 'ABC123',
-            username: 'El_Papi1502'
-    }
-    const user =getUser()
-    
-    expect(testUser).toEqual(user)
-     })
+  test("getUsuarioActivo debe retornar un objeto", () => {
+    const name = "Alex";
+    const user = getUsuarioActivo(name);
 
-     
- })
+    expect(user).toEqual({ uid: "ABC567", username: name });
+  });
+});
