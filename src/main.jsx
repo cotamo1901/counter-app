d03bc89dde99a0c65421013316e19914f0e07b3a
Alex <jcotamomoli@uniminuto.edu.co>
2023-10-09 14:37:14 -0500

    resolviendo erro

--- src/main.jsx
+++ src/main.jsx
@@ -2,21 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 // import { App } from "./HelloEorlApp";
 import { FirstApp } from "./FirstApp";
-<<<<<<< HEAD
-import {CounterApp} from "./CounterApp"
-=======
 // import {CounterApp} from "./CounterApp"
->>>>>>> 60d6951 (prueba)
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* <CounterApp value={0}/> */}
-<<<<<<< HEAD
-    <FirstApp  title="Hola,Soy John"/>
-=======
     <FirstApp  title="John Cotamo"/>
->>>>>>> 60d6951 (prueba)
     {/* <App/> */}
   </React.StrictMode>
 );
